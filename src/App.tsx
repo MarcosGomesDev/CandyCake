6ad1718055cc7e8e0f801870449ba2becb6e82e9
Marcos Gomes <marcosgomesdev@gmail.com>
2022-11-25 19:38:43 -0300

    Inicialização do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Title } from './styles';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <Title>Ola mundo</Title>
+        </>
+    );
+}
+
+export default App;
